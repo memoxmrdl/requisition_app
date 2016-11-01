e762da427240319a1c9c539a143fb9008aeb4f1f
memoxmrdl <jmemox@gmail.com>
2016-11-01 00:33:21 -0600

    Crear modelo Profile & aplicar relacion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :profile, dependent: :destroy
 end
---
+++ db/migrate/20161101062533_create_profiles.rb
@@ -0,0 +1,18 @@
+class CreateProfiles < ActiveRecord::Migration
+  def change
+    create_table :profiles do |t|
+      t.string :first_name
+      t.string :second_name
+      t.string :second_last_name
+      t.string :first_last_name
+      t.date :birth_date
+      t.string :curp
+      t.string :rfc
+      t.integer :gender
+      t.integer :birth_state
+      t.string :phone_number
+      t.belongs_to :user, index: true
+      t.timestamps null: false
+    end
+  end
+end
