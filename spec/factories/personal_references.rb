31f5c0b4fd2f883c8d11ee654c7a2f4db2cf47b3
memoxmrdl <jmemox@gmail.com>
2016-11-02 11:40:18 -0600

    considerar nueva validacion para PersonalReference requeridos los apellidos

--- spec/factories/personal_references.rb
+++ spec/factories/personal_references.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :personal_reference do
     first_name { Faker::Name.name }
     second_name { Faker::Name.name }
+    first_last_name { Faker::Name.name }
+    second_last_name { Faker::Name.name }
     cell_phone_number { Faker::PhoneNumber.cell_phone }
   end
 end
